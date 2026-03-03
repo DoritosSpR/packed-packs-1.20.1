001d8392394ec19ee9e6fd5e82f269be1a63f101
DoritosSpR <122171434+DoritosSpR@users.noreply.github.com>
2026-03-03 15:20:37 +0000

    Update GuiRectangle.java

--- src/main/java/io/github/fishstiz/fidgetz/gui/shapes/GuiRectangle.java
+++ src/main/java/io/github/fishstiz/fidgetz/gui/shapes/GuiRectangle.java
@@ -1,6 +1,7 @@
 package io.github.fishstiz.fidgetz.gui.shapes;
 
 import net.minecraft.client.gui.navigation.ScreenRectangle;
+import net.minecraft.client.gui.layouts.LayoutElement;
 import net.minecraft.util.Mth;
 
 public record GuiRectangle(int x, int y, int width, int height) {
@@ -9,14 +10,18 @@ public record GuiRectangle(int x, int y, int width, int height) {
         return new GuiRectangle(x, y, width, height);
     }
 
-    // Si usas el sistema de Layout de Minecraft 1.20.1
-    public static GuiRectangle viewOf(net.minecraft.client.gui.layouts.LayoutElement element) {
+    public static GuiRectangle viewOf(LayoutElement element) {
         return new GuiRectangle(element.getX(), element.getY(), element.getWidth(), element.getHeight());
     }
 
     public boolean containsPoint(double mouseX, double mouseY) {
         return mouseX >= (double)this.x && mouseX < (double)(this.x + this.width) 
             && mouseY >= (double)this.y && mouseY < (double)(this.y + this.height);
+    }
+
+    public boolean contains(GuiRectangle other) {
+        return other.x >= this.x && (other.x + other.width) <= (this.x + this.width)
+            && other.y >= this.y && (other.y + other.height) <= (this.y + this.height);
     }
 
     public ScreenRectangle getScreenRectangle() {
