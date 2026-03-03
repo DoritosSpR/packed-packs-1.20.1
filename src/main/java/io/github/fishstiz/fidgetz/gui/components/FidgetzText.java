8237d7ce0332026deac679f424255b7184d79f17
DoritosSpR <122171434+DoritosSpR@users.noreply.github.com>
2026-03-03 18:44:21 +0000

    Update FidgetzText.java

--- src/main/java/io/github/fishstiz/fidgetz/gui/components/FidgetzText.java
+++ src/main/java/io/github/fishstiz/fidgetz/gui/components/FidgetzText.java
@@ -1,28 +1,45 @@
 package io.github.fishstiz.fidgetz.gui.components;
 
-import io.github.fishstiz.fidgetz.gui.Metadata;
-import io.github.fishstiz.fidgetz.gui.shapes.GuiRectangle;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.Font;
 import net.minecraft.client.gui.GuiGraphics;
-import net.minecraft.client.gui.components.StringWidget;
+import net.minecraft.client.gui.components.AbstractWidget;
+import net.minecraft.client.gui.narration.NarrationElementOutput;
 import net.minecraft.network.chat.Component;
 
-public class FidgetzText<E> extends StringWidget implements Fidgetz, Metadata<E> {
-    private E metadata;
+public class FidgetzText<E> extends AbstractWidget implements Fidgetz {
+    private final boolean shadow;
+    private final int offsetY;
 
-    public FidgetzText(int x, int y, int width, int height, Component message, net.minecraft.client.gui.Font font) {
-        super(x, y, width, height, message, font);
+    private FidgetzText(Builder<E> builder) {
+        super(builder.x, builder.y, builder.width, builder.height, builder.message);
+        this.shadow = builder.shadow;
+        this.offsetY = builder.offsetY;
     }
 
     @Override
-    public GuiRectangle getViewRectangle() {
-        return new GuiRectangle(this.getX(), this.getY(), this.getWidth(), this.getHeight());
+    protected void renderWidget(GuiGraphics guiGraphics, int mouseX, int mouseY, float partialTick) {
+        Font font = Minecraft.getInstance().font;
+        int color = this.active ? 0xFFFFFF : 0xA0A0A0;
+        guiGraphics.drawString(font, this.getMessage(), this.getX(), this.getY() + offsetY, color, shadow);
     }
 
     @Override
-    public void renderWidget(GuiGraphics guiGraphics, int mouseX, int mouseY, float partialTick) {
-        super.renderWidget(guiGraphics, mouseX, mouseY, partialTick);
+    protected void updateWidgetNarration(NarrationElementOutput narration) {}
+
+    public static <E> Builder<E> builder() {
+        return new Builder<>();
     }
 
-    @Override public E getMetadata() { return metadata; }
-    @Override public void setMetadata(E metadata) { this.metadata = metadata; }
+    public static class Builder<E> extends AbstractWidgetBuilder<Builder<E>> {
+        protected boolean shadow = true;
+        protected int offsetY = 0;
+
+        public Builder<E> setShadow(boolean shadow) { this.shadow = shadow; return self(); }
+        public Builder<E> setOffsetY(int offsetY) { this.offsetY = offsetY; return self(); }
+
+        public FidgetzText<E> build() {
+            return new FidgetzText<>(this);
+        }
+    }
 }
