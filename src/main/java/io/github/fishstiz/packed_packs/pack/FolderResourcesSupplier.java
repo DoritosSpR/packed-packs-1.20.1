35dbe063b79f96383c664d47ee784cdd4c11e40f
DoritosSpR <122171434+DoritosSpR@users.noreply.github.com>
2026-03-03 17:55:16 +0000

    Update FolderResourcesSupplier.java

--- src/main/java/io/github/fishstiz/packed_packs/pack/FolderResourcesSupplier.java
+++ src/main/java/io/github/fishstiz/packed_packs/pack/FolderResourcesSupplier.java
@@ -1,18 +1,19 @@
-package io.github.fishstiz.packed_packs.pack.folder;
+package io.github.fishstiz.packed_packs.pack;
 
-import net.minecraft.server.packs.repository.Pack;
 import net.minecraft.server.packs.PackResources;
-import org.jetbrains.annotations.NotNull;
-
+import net.minecraft.server.packs.FilePackResources;
+import net.minecraft.server.packs.repository.Pack;
 import java.nio.file.Path;
 
-// En 1.20.1, Pack.ResourcesSupplier suele ser una interfaz funcional simple 
-// que devuelve un PackResources a partir de un String (el ID)
-public record FolderResourcesSupplier(Path path) implements Pack.ResourcesSupplier {
+public record FolderResourcesSupplier(String id, Path path) implements Pack.ResourcesSupplier {
+    @Override
+    public PackResources open(String id) {
+        // En 1.20.1 usamos la implementación nativa para carpetas/archivos
+        return new FilePackResources(this.id, this.path.toFile(), false);
+    }
 
     @Override
-    public @NotNull PackResources open(String id) {
-        // En 1.20.1, el método se llama 'open' y recibe el ID como String
-        return new FolderResources(id, this.path);
+    public PackResources openFull(String id, Pack.Info info) {
+        return this.open(id);
     }
 }
