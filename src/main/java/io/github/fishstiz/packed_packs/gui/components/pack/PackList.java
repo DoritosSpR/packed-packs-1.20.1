33818df91a2573bb488d1154885fc60b524f5e75
DoritosSpR <122171434+DoritosSpR@users.noreply.github.com>
2026-03-03 16:05:27 +0000

    Update PackList.java

--- src/main/java/io/github/fishstiz/packed_packs/gui/components/pack/PackList.java
+++ src/main/java/io/github/fishstiz/packed_packs/gui/components/pack/PackList.java
@@ -3,10 +3,7 @@ package io.github.fishstiz.packed_packs.gui.components.pack;
 import io.github.fishstiz.fidgetz.gui.components.AbstractFixedListWidget;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.components.ContainerObjectSelectionList;
-import net.minecraft.client.gui.components.events.GuiEventListener;
 import net.minecraft.server.packs.repository.Pack;
-import java.util.List;
-import java.util.Collections;
 
 public abstract class PackList extends AbstractFixedListWidget<PackList.Entry> {
     protected final int itemHeight;
@@ -24,34 +21,14 @@ public abstract class PackList extends AbstractFixedListWidget<PackList.Entry> {
     }
 
     public int getRowIndex(double mouseY) {
-    return (int)((mouseY - (double)this.getY() + this.getScrollAmount() - (double)this.headerHeight) / (double)this.itemHeight);
-}
+        // Usamos getY() en lugar de this.top
+        return (int)((mouseY - (double)this.getY() + this.getScrollAmount() - (double)this.headerHeight) / (double)this.itemHeight);
+    }
 
     public abstract class Entry extends ContainerObjectSelectionList.Entry<Entry> {
         protected final int index;
-
-        public Entry(int index) {
-            this.index = index;
-        }
-
+        public Entry(int index) { this.index = index; }
         public abstract Pack pack();
-
-        public boolean canOperateFile() {
-            return true;
-        }
-
-        @Override
-        public abstract void render(GuiGraphics g, int index, int top, int left, int width, int height, int mouseX, int mouseY, boolean isHovered, float partialTick);
-
-        @Override
-        public List<? extends GuiEventListener> children() {
-            return Collections.emptyList();
-        }
-
-        public int getX() { return PackList.this.getRowLeft(); }
-        public int getY() { return PackList.this.getRowTop(this.index); }
-        public int getWidth() { return PackList.this.getRowWidth(); }
-        public int getHeight() { return PackList.this.itemHeight; }
-        public int getBottom() { return this.getY() + this.getHeight(); }
+        @Override public abstract void render(GuiGraphics g, int index, int top, int left, int width, int height, int mouseX, int mouseY, boolean isHovered, float partialTick);
     }
 }
