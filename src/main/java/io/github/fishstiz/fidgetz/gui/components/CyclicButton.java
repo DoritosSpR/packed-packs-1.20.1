cc70cd48d06dc0241c3d989a3c854b16be332cc4
DoritosSpR <122171434+DoritosSpR@users.noreply.github.com>
2026-03-03 18:44:35 +0000

    Update CyclicButton.java

--- src/main/java/io/github/fishstiz/fidgetz/gui/components/CyclicButton.java
+++ src/main/java/io/github/fishstiz/fidgetz/gui/components/CyclicButton.java
@@ -1,41 +1,29 @@
 package io.github.fishstiz.fidgetz.gui.components;
 
-import net.minecraft.client.gui.GuiGraphics;
-import net.minecraft.client.gui.components.Button;
 import net.minecraft.network.chat.Component;
-import io.github.fishstiz.fidgetz.gui.renderables.sprites.Sprite;
-
+import net.minecraft.resources.ResourceLocation;
 import java.util.List;
 
-public class CyclicButton<T> extends Button {
-    private final List<T> values;
-    private int index;
-
-    public CyclicButton(int x, int y, int width, int height, Component message, OnPress onPress, List<T> values) {
-        super(x, y, width, height, message, onPress, DEFAULT_NARRATION);
-        this.values = values;
-        this.index = 0;
+public class CyclicButton<T, E> extends FidgetzButton<E> {
+    
+    public interface SpriteOption {
+        Component getMessage();
+        ResourceLocation getSprite();
     }
 
-    public T getValue() {
-        return values.get(index);
+    protected CyclicButton(Builder<T, E> builder) {
+        super(builder);
     }
 
-    public void next() {
-        index = (index + 1) % values.size();
+    // El error principal era que el código buscaba esta interfaz dentro de CyclicButton
+    public interface CyclicValue {
+        Component getMessage();
     }
 
-    private boolean hasCustomSprite() {
-        return this.getValue() instanceof Sprite;
-    }
-
-    @Override
-    public void renderWidget(GuiGraphics guiGraphics, int mouseX, int mouseY, float partialTick) {
-        super.renderWidget(guiGraphics, mouseX, mouseY, partialTick);
-        
-        if (hasCustomSprite()) {
-            Sprite sprite = (Sprite) getValue();
-            sprite.render(guiGraphics, getX() + 2, getY() + 2, width - 4, height - 4);
+    public static class Builder<T, E> extends FidgetzButton.Builder<E> {
+        // Implementación mínima para que AvailablePacksLayout compile
+        public CyclicButton<T, E> build() {
+            return new CyclicButton<>(this);
         }
     }
 }
