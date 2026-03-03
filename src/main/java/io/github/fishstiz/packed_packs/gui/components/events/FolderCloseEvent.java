2d33c8cc97567dded577e750251c95b2501f5eb7
DoritosSpR <122171434+DoritosSpR@users.noreply.github.com>
2026-03-03 16:38:13 +0000

    Update FolderCloseEvent.java

--- src/main/java/io/github/fishstiz/packed_packs/gui/components/events/FolderCloseEvent.java
+++ src/main/java/io/github/fishstiz/packed_packs/gui/components/events/FolderCloseEvent.java
@@ -1,7 +1,7 @@
 package io.github.fishstiz.packed_packs.gui.components.events;
 
 import io.github.fishstiz.packed_packs.gui.components.pack.FolderPackList;
-import io.github.fishstiz.packed_packs.pack.folder.FolderPack;
+import io.github.fishstiz.packed_packs.pack.FolderPack;
 
 public record FolderCloseEvent(FolderPackList target, FolderPack folderPack) implements FileEvent {
 }
