6480c554f53d1ea1a2b707b4834748e7fc8ff530
DoritosSpR <122171434+DoritosSpR@users.noreply.github.com>
2026-03-03 15:20:53 +0000

    Update PackList.java

--- src/main/java/io/github/fishstiz/packed_packs/gui/components/pack/PackList.java
+++ src/main/java/io/github/fishstiz/packed_packs/gui/components/pack/PackList.java
@@ -4,6 +4,7 @@ import io.github.fishstiz.fidgetz.gui.components.AbstractFixedListWidget;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.components.ContainerObjectSelectionList;
 import net.minecraft.client.gui.components.events.GuiEventListener;
+import net.minecraft.server.packs.repository.Pack;
 import java.util.List;
 import java.util.Collections;
 
@@ -15,23 +16,29 @@ public abstract class PackList extends AbstractFixedListWidget<PackList.Entry> {
         this.itemHeight = itemHeight;
     }
 
-    // Dentro de tu clase PackList, asegúrate de que Entry tenga esto:
-public abstract class Entry extends ContainerObjectSelectionList.Entry<Entry> {
-    // ... (constructor y métodos anteriores)
-
-    public abstract net.minecraft.server.packs.repository.Pack pack();
-
-    public boolean canOperateFile() {
-        return true; // O tu lógica de permisos
+    public Entry getEntry(Pack pack) {
+        for (Entry entry : this.children()) {
+            if (entry.pack().equals(pack)) return entry;
+        }
+        return null;
     }
 
-    // Para 1.20.1, el sistema de widgets es diferente. 
-    // Si usas MixinExtras o una librería personalizada, asegúrate de que este método exista:
-    public <T extends net.minecraft.client.gui.components.events.GuiEventListener & net.minecraft.client.gui.components.Renderable> T addWidget(T widget) {
-        // Lógica para añadir el botón de hamburguesa/carpeta al renderizado de la línea
-        return widget;
+    public int getRowIndex(double mouseY) {
+        return (int)((mouseY - (double)this.top + this.getScrollAmount() - (double)this.headerHeight) / (double)this.itemHeight);
     }
-}
+
+    public abstract class Entry extends ContainerObjectSelectionList.Entry<Entry> {
+        protected final int index;
+
+        public Entry(int index) {
+            this.index = index;
+        }
+
+        public abstract Pack pack();
+
+        public boolean canOperateFile() {
+            return true;
+        }
 
         @Override
         public abstract void render(GuiGraphics g, int index, int top, int left, int width, int height, int mouseX, int mouseY, boolean isHovered, float partialTick);
@@ -41,7 +48,6 @@ public abstract class Entry extends ContainerObjectSelectionList.Entry<Entry> {
             return Collections.emptyList();
         }
 
-        // Métodos de compatibilidad para que otros archivos encuentren las coordenadas
         public int getX() { return PackList.this.getRowLeft(); }
         public int getY() { return PackList.this.getRowTop(this.index); }
         public int getWidth() { return PackList.this.getRowWidth(); }
