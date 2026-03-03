6d6399e13dbd5d3f5068c8291d5c8c6348cbe0c1
DoritosSpR <122171434+DoritosSpR@users.noreply.github.com>
2026-03-03 18:31:58 +0000

    Update Fidgetz.java

--- src/main/java/io/github/fishstiz/fidgetz/gui/components/Fidgetz.java
+++ src/main/java/io/github/fishstiz/fidgetz/gui/components/Fidgetz.java
@@ -1,15 +1,15 @@
 package io.github.fishstiz.fidgetz.gui.components;
 
-import net.minecraft.client.gui.navigation.ScreenRectangle;
 import io.github.fishstiz.fidgetz.gui.util.GuiRectangle;
+import net.minecraft.client.gui.navigation.ScreenRectangle;
 
 public interface Fidgetz {
-    default ScreenRectangle getScreenRectangle(GuiRectangle rect) {
-        return new ScreenRectangle(rect.x(), rect.y(), rect.width(), rect.height());
-    }
-
     int getX();
     int getY();
     int getWidth();
     int getHeight();
+
+    default ScreenRectangle getScreenRectangle(GuiRectangle rect) {
+        return new ScreenRectangle(rect.x(), rect.y(), rect.width(), rect.height());
+    }
 }
