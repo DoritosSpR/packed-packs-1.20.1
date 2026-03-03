9d55416f725f1ff2c862559ce613baa2ecc5c4a8
DoritosSpR <122171434+DoritosSpR@users.noreply.github.com>
2026-03-03 18:38:24 +0000

    Update AbstractWidgetBuilder.java

--- src/main/java/io/github/fishstiz/fidgetz/gui/components/AbstractWidgetBuilder.java
+++ src/main/java/io/github/fishstiz/fidgetz/gui/components/AbstractWidgetBuilder.java
@@ -1,57 +1,53 @@
 package io.github.fishstiz.fidgetz.gui.components;
 
-import org.jetbrains.annotations.NotNull;
+import net.minecraft.network.chat.Component;
 
-public abstract class AbstractWidgetBuilder<B> {
-    public static final int DEFAULT_WIDTH = 150;
-    public static final int DEFAULT_HEIGHT = 20;
+public abstract class AbstractWidgetBuilder<B extends AbstractWidgetBuilder<B>> {
     protected int x;
     protected int y;
-    protected int width = DEFAULT_WIDTH;
-    protected int height = DEFAULT_HEIGHT;
+    protected int width;
+    protected int height;
+    protected Component message = Component.empty();
 
     @SuppressWarnings("unchecked")
     protected B self() {
         return (B) this;
     }
 
-    public @NotNull B setX(int x) {
+    public B setX(int x) {
         this.x = x;
-        return this.self();
+        return self();
     }
 
-    public @NotNull B setY(int y) {
+    public B setY(int y) {
         this.y = y;
-        return this.self();
+        return self();
     }
 
-    public @NotNull B setPosition(int x, int y) {
+    public B setPosition(int x, int y) {
         this.x = x;
         this.y = y;
-        return this.self();
+        return self();
     }
 
-    public @NotNull B setWidth(int width) {
+    public B setWidth(int width) {
         this.width = width;
-        return this.self();
+        return self();
     }
 
-    public @NotNull B setHeight(int height) {
+    public B setHeight(int height) {
         this.height = height;
-        return this.self();
+        return self();
     }
 
-    public @NotNull B setDimensions(int width, int height) {
-        this.setWidth(width);
-        this.setHeight(height);
-        return this.self();
+    public B setSize(int width, int height) {
+        this.width = width;
+        this.height = height;
+        return self();
     }
 
-    public @NotNull B makeSquare(int size) {
-        return this.setDimensions(size, size);
-    }
-
-    public @NotNull B makeSquare() {
-        return this.makeSquare(this.height);
+    public B setMessage(Component message) {
+        this.message = message;
+        return self();
     }
 }
