31fd8f988fc40459817baac1340999cb3f8caa4f
DoritosSpR <122171434+DoritosSpR@users.noreply.github.com>
2026-03-03 16:05:56 +0000

    Update FolderPackList.java

--- src/main/java/io/github/fishstiz/packed_packs/gui/components/pack/FolderPackList.java
+++ src/main/java/io/github/fishstiz/packed_packs/gui/components/pack/FolderPackList.java
@@ -5,15 +5,17 @@ import java.util.function.Consumer;
 import net.minecraft.client.gui.components.AbstractWidget;
 
 public class FolderPackList extends PackList implements LayoutElement {
-    // Agregamos el constructor que falta
+    public boolean visible = true;
+
+    // Ajustamos el constructor para recibir lo que FolderDialog intenta pasarle
     public FolderPackList(Object options, Object assets, Object fileOps, Object screen) {
-        super(200, 200, 0, 200, 36); // Valores por defecto
+        super(200, 200, 40, 200, 36); 
     }
 
-    @Override public void setX(int x) { this.left = x; }
-    @Override public void setY(int y) { this.top = y; }
-    @Override public int getX() { return this.left; }
-    @Override public int getY() { return this.top; }
+    @Override public void setX(int x) { this.setLeft(x); }
+    @Override public void setY(int y) { /* En 1.20.1 esto se maneja distinto en listas */ }
+    @Override public int getX() { return this.getRowLeft(); }
+    @Override public int getY() { return 0; } // Ajustar según necesidad
     @Override public int getWidth() { return this.width; }
     @Override public int getHeight() { return this.height; }
     @Override public void visitWidgets(Consumer<AbstractWidget> consumer) {}
