762e8d43e657d3b01109840f5df71fb7bda3cf3b
DoritosSpR <122171434+DoritosSpR@users.noreply.github.com>
2026-03-03 18:32:41 +0000

    Update ResourceUtil.java

--- src/main/java/io/github/fishstiz/packed_packs/util/ResourceUtil.java
+++ src/main/java/io/github/fishstiz/packed_packs/util/ResourceUtil.java
@@ -1,26 +1,20 @@
 package io.github.fishstiz.packed_packs.util;
 
-import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 
 public class ResourceUtil {
     public static final String MOD_ID = "packed_packs";
-    public static final String MOD_NAME = "Packed Packs";
 
-    public static Component getText(String key) {
-        return Component.translatable(MOD_ID + "." + key);
-    }
-
-    public static ResourceLocation getGuiSprite(String path) {
-        // En 1.20.1 se usa el constructor de ResourceLocation directamente
-        return new ResourceLocation(MOD_ID, "textures/gui/sprites/" + path + ".png");
-    }
-
-    public static ResourceLocation getVanillaSprite(String path) {
-        return new ResourceLocation("minecraft", "textures/gui/sprites/" + path + ".png");
-    }
-
-    public static ResourceLocation fromPath(String path) {
+    public static ResourceLocation id(String path) {
         return new ResourceLocation(MOD_ID, path);
+    }
+
+    public static ResourceLocation getIcon(String name) {
+        // En 1.20.1 los sprites de la GUI suelen ir en esta ruta
+        return new ResourceLocation(MOD_ID, "textures/gui/sprites/" + name + ".png");
+    }
+    
+    public static ResourceLocation getGuiSprite(String name) {
+        return getIcon(name);
     }
 }
