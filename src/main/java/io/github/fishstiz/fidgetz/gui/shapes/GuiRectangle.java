8e8d9f189b8701b8ac500e3b644e3404c19ca64c
DoritosSpR <122171434+DoritosSpR@users.noreply.github.com>
2026-03-03 18:30:50 +0000

    Delete src/main/java/io/github/fishstiz/fidgetz/gui/shapes/GuiRectangle.java

--- src/main/java/io/github/fishstiz/fidgetz/gui/shapes/GuiRectangle.java
+++
@@ -1,8 +0,0 @@
-package io.github.fishstiz.fidgetz.gui.shapes;
-
-public record GuiRectangle(int x, int y, int width, int height) {
-    public int left() { return x; }
-    public int top() { return y; }
-    public int right() { return x + width; }
-    public int bottom() { return y + height; }
-}
