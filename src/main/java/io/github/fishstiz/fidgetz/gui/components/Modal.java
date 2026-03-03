983e0eaa1a19ed387ccb19073f35d0ce6d1081fb
DoritosSpR <122171434+DoritosSpR@users.noreply.github.com>
2026-03-03 18:27:02 +0000

    Update Modal.java

--- src/main/java/io/github/fishstiz/fidgetz/gui/components/Modal.java
+++ src/main/java/io/github/fishstiz/fidgetz/gui/components/Modal.java
@@ -1,19 +1,23 @@
 package io.github.fishstiz.fidgetz.gui.components;
 
 import io.github.fishstiz.fidgetz.gui.shapes.Padding;
+import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.layouts.Layout;
 import net.minecraft.client.gui.screens.Screen;
-
-import static io.github.fishstiz.fidgetz.util.DrawUtil.DEMO_BACKGROUND;
 
 public class Modal<T extends Layout> extends ToggleableDialog<LayoutWrapper<T>> {
     private static final int MIN_SIZE = 50;
 
     protected Modal(Builder<T> builder) {
         super(builder);
-
         this.root().setPadding(builder.padding);
-        this.root().visitWidgets(this::addRenderableWidget);
+    }
+
+    @Override
+    public void render(GuiGraphics guiGraphics, int mouseX, int mouseY, float partialTick) {
+        if (!isOpen()) return;
+        // Renderizar fondo y luego el layout
+        this.root().visitWidgets(widget -> widget.render(guiGraphics, mouseX, mouseY, partialTick));
     }
 
     public void repositionElements() {
@@ -22,13 +26,7 @@ public class Modal<T extends Layout> extends ToggleableDialog<LayoutWrapper<T>> 
         this.root().setY(this.screen.height / 2 - this.root().getHeight() / 2);
     }
 
-    public void closeModal() {
-        this.setOpen(false);
-    }
-
-    public void clear() {
-        this.clearWidgets();
-    }
+    public void closeModal() { this.setOpen(false); }
 
     public static <S extends Screen & ToggleableDialogContainer, T extends Layout> Builder<T> builder(S screen, T layout) {
         return new Builder<>(screen, new LayoutWrapper<>(layout, MIN_SIZE, MIN_SIZE));
@@ -37,19 +35,13 @@ public class Modal<T extends Layout> extends ToggleableDialog<LayoutWrapper<T>> 
     public static class Builder<T extends Layout> extends ToggleableDialog.Builder<LayoutWrapper<T>, Builder<T>> {
         protected Padding padding = Padding.empty();
 
-        protected <S extends Screen & ToggleableDialogContainer> Builder(S screen, LayoutWrapper<T> root) {
+        public Builder(Screen screen, LayoutWrapper<T> root) {
             super(screen, root);
-
-            this.background = DEMO_BACKGROUND;
         }
 
         public Builder<T> padding(Padding padding) {
             this.padding = padding;
             return this;
-        }
-
-        public Builder<T> padding(int padding) {
-            return this.padding(new Padding(padding));
         }
 
         @Override
