cbc7b46c6fb3ba26645e381ad822f8a8794d371b
DoritosSpR <122171434+DoritosSpR@users.noreply.github.com>
2026-03-03 15:18:19 +0000

    Update FolderPackList.java

--- src/main/java/io/github/fishstiz/packed_packs/gui/components/pack/FolderPackList.java
+++ src/main/java/io/github/fishstiz/packed_packs/gui/components/pack/FolderPackList.java
@@ -1,54 +1,21 @@
 package io.github.fishstiz.packed_packs.gui.components.pack;
 
-import io.github.fishstiz.packed_packs.gui.components.SelectionContext;
-import io.github.fishstiz.packed_packs.gui.components.events.*;
-import io.github.fishstiz.packed_packs.pack.PackAssetManager;
-import io.github.fishstiz.packed_packs.pack.PackFileOperations;
-import io.github.fishstiz.packed_packs.pack.PackOptionsContext;
-import net.minecraft.client.gui.GuiGraphics;
-import net.minecraft.server.packs.repository.Pack;
-import org.jetbrains.annotations.NotNull;
+import net.minecraft.client.gui.layouts.LayoutElement;
+import java.util.function.Consumer;
+import net.minecraft.client.gui.components.AbstractWidget;
 
-public class FolderPackList extends CurrentPackList {
-    public FolderPackList(PackOptionsContext options, PackAssetManager assets, PackFileOperations fileOps, PackListEventListener listener) {
-        super(options, assets, fileOps, listener);
-    }
+// Añadimos LayoutElement para que sea compatible con ToggleableDialog
+public class FolderPackList extends PackList implements LayoutElement {
+    public boolean visible = true;
 
-    @Override
-    protected @NotNull Entry createEntry(SelectionContext<Pack> context, int index) {
-        return new SubPackEntry(context, index);
-    }
-
-    @Override
-    public boolean isTransferable(Pack pack) {
-        return false;
-    }
-
-    @Override
-    public boolean canInteract(PackList source) {
-        return source == this;
-    }
-
-    @Override
-    public boolean canDrop(DragEvent dragEvent, double mouseX, double mouseY) {
-        return this.canInteract(dragEvent.target()) && super.canDrop(dragEvent, mouseX, mouseY);
-    }
-
-    @Override
-    public void renderDroppableZone(GuiGraphics guiGraphics, DragEvent dragEvent, int mouseX, int mouseY, float partialTick) {
-        if (dragEvent.target() == this) {
-            super.renderDroppableZone(guiGraphics, dragEvent, mouseX, mouseY, partialTick);
-        }
-    }
-
-    protected class SubPackEntry extends Entry {
-        protected SubPackEntry(SelectionContext<Pack> context, int index) {
-            super(context, index);
-        }
-
-        @Override
-        public boolean isTransferable() {
-            return false;
-        }
+    // Implementación manual de LayoutElement para 1.20.1
+    @Override public void setX(int x) { this.left = x; }
+    @Override public void setY(int y) { this.top = y; }
+    @Override public int getX() { return this.left; }
+    @Override public int getY() { return this.top; }
+    @Override public int getWidth() { return this.width; }
+    @Override public int getHeight() { return this.height; }
+    @Override public void visitWidgets(Consumer<AbstractWidget> consumer) {
+        // Si la lista tiene widgets internos (botones), pásalos aquí
     }
 }
