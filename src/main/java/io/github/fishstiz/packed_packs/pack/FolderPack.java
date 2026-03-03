1407dd73940b392920f05dee40c93502f48ef927
DoritosSpR <122171434+DoritosSpR@users.noreply.github.com>
2026-03-03 16:46:50 +0000

    Update FolderPack.java

--- src/main/java/io/github/fishstiz/packed_packs/pack/FolderPack.java
+++ src/main/java/io/github/fishstiz/packed_packs/pack/FolderPack.java
@@ -3,13 +3,14 @@ package io.github.fishstiz.packed_packs.pack;
 import io.github.fishstiz.packed_packs.util.PackUtil;
 import net.minecraft.network.chat.Component;
 import net.minecraft.server.packs.repository.Pack;
+import net.minecraft.server.packs.PackType;
 import java.util.List;
 
 public abstract class FolderPack extends Pack {
     
     public FolderPack(String id, boolean required, ResourcesSupplier resources, Component title, Info info, Position position, boolean fixed, boolean hidden) {
-        // En 1.20.1 el orden es: id, fixed, resources, title, info, compatibility, position, hidden, source
-        super(id, fixed, resources, title, info, info.compatibility(), position, hidden, PackUtil.PACK_SOURCE);
+        // Corregido: info.compatibility ahora requiere el PackType (usualmente CLIENT_RESOURCES o SERVER_DATA)
+        super(id, fixed, resources, title, info, info.compatibility(PackType.CLIENT_RESOURCES), position, hidden, PackUtil.PACK_SOURCE);
     }
 
     public abstract List<Pack> flatten();
