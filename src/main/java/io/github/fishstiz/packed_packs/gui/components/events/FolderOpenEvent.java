72fe10cefedc7e4eb999a931723aa7a0121b5caf
DoritosSpR <122171434+DoritosSpR@users.noreply.github.com>
2026-03-03 16:37:58 +0000

    Update FolderOpenEvent.java

--- src/main/java/io/github/fishstiz/packed_packs/gui/components/events/FolderOpenEvent.java
+++ src/main/java/io/github/fishstiz/packed_packs/gui/components/events/FolderOpenEvent.java
@@ -1,7 +1,7 @@
 package io.github.fishstiz.packed_packs.gui.components.events;
 
 import io.github.fishstiz.packed_packs.gui.components.pack.PackList;
-import io.github.fishstiz.packed_packs.pack.folder.FolderPack;
+import io.github.fishstiz.packed_packs.pack.FolderPack;
 
 public record FolderOpenEvent(PackList target, FolderPack opened) implements FileEvent {
 }
