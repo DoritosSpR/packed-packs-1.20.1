794938b16e43776653a3fd0a445dd14740919a8e
DoritosSpR <122171434+DoritosSpR@users.noreply.github.com>
2026-03-03 16:10:42 +0000

    Update Fidgetz.java

--- src/main/java/io/github/fishstiz/fidgetz/gui/components/Fidgetz.java
+++ src/main/java/io/github/fishstiz/fidgetz/gui/components/Fidgetz.java
@@ -3,11 +3,21 @@ package io.github.fishstiz.fidgetz.gui.components;
 import io.github.fishstiz.fidgetz.gui.shapes.GuiRectangle;
 import net.minecraft.client.gui.components.events.GuiEventListener;
 import net.minecraft.client.gui.layouts.LayoutElement;
+import net.minecraft.client.gui.navigation.ScreenRectangle;
 
 public interface Fidgetz extends GuiEventListener, LayoutElement {
-    // Cambiamos el nombre a getViewRectangle para evitar el choque con Minecraft 1.20.1
+    // Esto resuelve el conflicto devolviendo el tipo que Minecraft espera,
+    // pero usando los datos de nuestro GuiRectangle interno.
+    @Override
+    default ScreenRectangle getRectangle() {
+        GuiRectangle rect = getViewRectangle();
+        return new ScreenRectangle(rect.getX(), rect.getY(), rect.getWidth(), rect.getHeight());
+    }
+
+    // Este es nuestro método propio que las clases deben implementar
     GuiRectangle getViewRectangle();
 
+    @Override
     default boolean isMouseOver(double mouseX, double mouseY) {
         return getViewRectangle().containsPoint(mouseX, mouseY);
     }
