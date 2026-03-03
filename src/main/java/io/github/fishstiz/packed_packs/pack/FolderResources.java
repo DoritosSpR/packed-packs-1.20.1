77602e97e010b0bb18b3ca97e32ead8397a8a9ee
DoritosSpR <122171434+DoritosSpR@users.noreply.github.com>
2026-03-03 17:18:12 +0000

    Update FolderResources.java

--- src/main/java/io/github/fishstiz/packed_packs/pack/FolderResources.java
+++ src/main/java/io/github/fishstiz/packed_packs/pack/FolderResources.java
@@ -4,8 +4,6 @@ import net.minecraft.server.packs.PathPackResources;
 import java.nio.file.Path;
 
 public class FolderResources extends PathPackResources {
-    public static final String FOLDER_CONFIG_FILENAME = ".folder_config.json";
-
     public FolderResources(String id, Path path) {
         super(id, path, false);
     }
