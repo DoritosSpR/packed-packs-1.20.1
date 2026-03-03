ea07f6f1ef7ff360e838b0833332151be5a20da2
DoritosSpR <122171434+DoritosSpR@users.noreply.github.com>
2026-03-03 15:14:11 +0000

    Update PackedPacks.java

--- src/main/java/io/github/fishstiz/packed_packs/PackedPacks.java
+++ src/main/java/io/github/fishstiz/packed_packs/PackedPacks.java
@@ -2,17 +2,21 @@ package io.github.fishstiz.packed_packs;
 
 import io.github.fishstiz.packed_packs.impl.PackedPacksApiImpl;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.fml.loading.FMLPaths;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import java.nio.file.Path;
 
 @Mod("packed_packs")
 public class PackedPacks {
+    public static final String MOD_ID = "packed_packs";
+    public static final Logger LOGGER = LogManager.getLogger();
+
     public PackedPacks() {
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+        PackedPacksApiImpl.getInstance();
     }
 
-    private void setup(final FMLCommonSetupEvent event) {
-        // Ahora el símbolo es visible gracias al import
-        PackedPacksApiImpl.getInstance();
+    public static Path getConfigDir() {
+        return FMLPaths.CONFIGDIR.get().resolve(MOD_ID);
     }
 }
