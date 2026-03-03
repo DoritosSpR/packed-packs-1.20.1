8fee83151db0789c6e890704d35a12f9c60109ba
DoritosSpR <122171434+DoritosSpR@users.noreply.github.com>
2026-03-03 18:05:34 +0000

    Update Fidgetz.java

--- src/main/java/io/github/fishstiz/fidgetz/gui/components/Fidgetz.java
+++ src/main/java/io/github/fishstiz/fidgetz/gui/components/Fidgetz.java
@@ -3,14 +3,13 @@ package io.github.fishstiz.fidgetz.gui.components;
 import net.minecraft.client.gui.navigation.ScreenRectangle;
 import io.github.fishstiz.fidgetz.gui.util.GuiRectangle;
 
-public class Fidgetz {
-    
-    /**
-     * Convierte un GuiRectangle de Fidgetz a un ScreenRectangle de Minecraft.
-     */
-    public static ScreenRectangle getScreenRectangle(GuiRectangle rect) {
-        // En 1.20.1 los métodos de GuiRectangle suelen ser left(), top(), width() y height()
-        // o x(), y(), width(), height() dependiendo de tu implementación de Fidgetz.
+public interface Fidgetz {
+    default ScreenRectangle getScreenRectangle(GuiRectangle rect) {
         return new ScreenRectangle(rect.x(), rect.y(), rect.width(), rect.height());
     }
+
+    int getX();
+    int getY();
+    int getWidth();
+    int getHeight();
 }
