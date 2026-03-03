a3562d55bb75737c6940210869650b83e439f356
DoritosSpR <122171434+DoritosSpR@users.noreply.github.com>
2026-03-03 15:28:11 +0000

    Update AbstractLayoutElement.java

--- src/main/java/io/github/fishstiz/fidgetz/gui/components/AbstractLayoutElement.java
+++ src/main/java/io/github/fishstiz/fidgetz/gui/components/AbstractLayoutElement.java
@@ -1,61 +1,19 @@
 package io.github.fishstiz.fidgetz.gui.components;
 
 import io.github.fishstiz.fidgetz.gui.shapes.GuiRectangle;
-import net.minecraft.client.gui.components.AbstractWidget;
 import net.minecraft.client.gui.layouts.LayoutElement;
 
-import java.util.function.Consumer;
+public abstract class AbstractLayoutElement implements LayoutElement {
+    protected int x, y, width, height;
 
-public abstract class AbstractLayoutElement implements LayoutElement, GuiRectangle {
-    private int x;
-    private int y;
-    private int width;
-    private int height;
+    @Override public int getX() { return x; }
+    @Override public int getY() { return y; }
+    @Override public int getWidth() { return width; }
+    @Override public int getHeight() { return height; }
+    @Override public void setX(int x) { this.x = x; }
+    @Override public void setY(int y) { this.y = y; }
 
-    @Override
-    public void setX(int x) {
-        this.x = x;
-    }
-
-    @Override
-    public void setY(int y) {
-        this.y = y;
-    }
-
-    @Override
-    public int getX() {
-        return this.x;
-    }
-
-    @Override
-    public int getY() {
-        return this.y;
-    }
-
-    public void setWidth(int width) {
-        this.width = width;
-    }
-
-    public void setHeight(int height) {
-        this.height = height;
-    }
-
-    @Override
-    public int getWidth() {
-        return this.width;
-    }
-
-    @Override
-    public int getHeight() {
-        return this.height;
-    }
-
-    public void setSize(int width, int height) {
-        this.setWidth(width);
-        this.setHeight(height);
-    }
-
-    @Override
-    public void visitWidgets(Consumer<AbstractWidget> consumer) {
+    public GuiRectangle getRectangle() {
+        return new GuiRectangle(x, y, width, height);
     }
 }
