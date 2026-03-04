9cc58376e6bf1db3f450882ed2864d9472c01704
DoritosSpR <122171434+DoritosSpR@users.noreply.github.com>
2026-03-04 19:52:11 +0000

    Update GuiRectangle.java

--- src/main/java/io/github/fishstiz/fidgetz/gui/util/GuiRectangle.java
+++ src/main/java/io/github/fishstiz/fidgetz/gui/util/GuiRectangle.java
@@ -1,8 +1,10 @@
-package io.github.fishstiz.fidgetz.gui.util;
+package io.github.fishstiz.fidgetz.gui.shapes;
 
 public record GuiRectangle(int x, int y, int width, int height) {
-    public int left() { return x; }
-    public int top() { return y; }
+    public boolean contains(double mouseX, double mouseY) {
+        return mouseX >= (double)this.x && mouseX < (double)(this.x + this.width) && mouseY >= (double)this.y && mouseY < (double)(this.y + this.height);
+    }
+
     public int right() { return x + width; }
     public int bottom() { return y + height; }
 }
