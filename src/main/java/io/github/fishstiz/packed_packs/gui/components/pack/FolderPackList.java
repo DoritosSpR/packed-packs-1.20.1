d6a94997d09a2306fa2cd770033d965e7acb48f5
DoritosSpR <122171434+DoritosSpR@users.noreply.github.com>
2026-03-04 19:52:56 +0000

    Update FolderPackList.java

--- src/main/java/io/github/fishstiz/packed_packs/gui/components/pack/FolderPackList.java
+++ src/main/java/io/github/fishstiz/packed_packs/gui/components/pack/FolderPackList.java
@@ -1,43 +1,35 @@
 package io.github.fishstiz.packed_packs.gui.components.pack;
 
+import io.github.fishstiz.packed_packs.gui.components.events.PackListEventListener;
 import io.github.fishstiz.packed_packs.pack.PackAssetManager;
 import io.github.fishstiz.packed_packs.pack.PackFileOperations;
 import io.github.fishstiz.packed_packs.pack.PackOptionsContext;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.components.AbstractWidget;
-import net.minecraft.client.gui.layouts.LayoutElement;
-import net.minecraft.client.gui.navigation.ScreenRectangle;
-import net.minecraft.client.gui.screens.Screen;
-import java.util.function.Consumer;
+import net.minecraft.server.packs.repository.Pack;
+import java.util.List;
 
-public class FolderPackList extends PackList implements LayoutElement {
-    private int x, y, width, height;
+public class FolderPackList extends PackList {
 
-    public FolderPackList(PackOptionsContext options, PackAssetManager assets, PackFileOperations fileOps, Screen screen) {
-    // ELIMINA Minecraft.getInstance()
-    super(screen.width, screen.height, 40, screen.height - 40, 36); 
-    this.width = screen.width;
-    this.height = screen.height;
-}
-
-    @Override public void setX(int x) { this.x = x; this.setLeftPos(x); }
-    @Override public void setY(int y) { 
-        this.y = y; 
-        // Actualiza el área de scroll para que coincida con la posición del diálogo
-        this.updateSize(this.width, this.height, y, y + this.height);
+    public FolderPackList(Minecraft minecraft, int width, int height, int top, int bottom, 
+                          PackOptionsContext options, PackAssetManager assets, 
+                          PackFileOperations fileOps, PackListEventListener listener) {
+        super(minecraft, width, height, top, bottom, 36, options, assets, fileOps, listener);
     }
-    @Override public int getX() { return this.x; }
-    @Override public int getY() { return this.y; }
-    @Override public int getWidth() { return this.width; }
-    @Override public int getHeight() { return this.height; }
-    
-    public void setWidth(int width) { this.width = width; }
-    public void setHeight(int height) { this.height = height; }
 
     @Override
-    public ScreenRectangle getRectangle() {
-        return new ScreenRectangle(this.x, this.y, this.width, this.height);
-    }
-
-    public void visitWidgets(Consumer<AbstractWidget> consumer) { }
+    public boolean isTransferable(Pack pack) { return false; }
+    @Override
+    public void removeAll(List<Pack> packs) {}
+    @Override
+    public void addAll(List<Pack> packs) {}
+    @Override
+    public void select(Pack pack) {}
+    @Override
+    public void selectAll(List<Pack> packs) {}
+    @Override
+    public void clearSelection() {}
+    @Override
+    public Entry getEntry(Pack pack) { return null; }
+    @Override
+    public boolean canInteract(PackList source) { return false; }
 }
